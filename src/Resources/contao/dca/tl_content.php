9f6262964ebccac8d5a2d28281d69024b7e1a63b
Alexander Naumov <me@alexandernaumov.de>
2019-11-19 13:43:38 +0100

    change sql type

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -3,7 +3,7 @@
 $GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['search'] = true;
 $GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['default'] = 'h3';
 $GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['inputType'] = 'inputUnit';
-$GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['eval']['maxlength'] = 200;
+$GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['eval']['maxlength'] = 255;
 $GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['eval']['tl_class'] = 'w50 clr';
 $GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['options'] = [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ];
-$GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['sql'] = "varchar(255) NOT NULL default 'a:2:{s:5:\"value\";s:0:\"\";s:4:\"unit\";s:2:\"h3\";}'";
+$GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['sql'] = "text NOT NULL default 'a:2:{s:5:\"value\";s:0:\"\";s:4:\"unit\";s:2:\"h3\";}'";
