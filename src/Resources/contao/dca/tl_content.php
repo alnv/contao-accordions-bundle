d7662e054f687450eafcc2ad81be11e22f3bb4ae
Alexander Naumov <alexandernaumov@Alexander-Naumov-Geschaft.local>
2023-02-08 10:20:31 +0100

    change text and add span

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,11 +1,11 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['search'] = true;
-$GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['default'] = '';
+$GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['default'] = 'span';
 $GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['inputType'] = 'inputUnit';
 $GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['eval']['maxlength'] = 2048;
 $GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['eval']['tl_class'] = 'w50 clr';
-$GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['options'] = [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ];
+$GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['options'] = ['span', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
 $GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['sql'] = "text NOT NULL";
 
 \Contao\CoreBundle\DataContainer\PaletteManipulator::create()
@@ -17,7 +17,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['mooHeadline']['sql'] = "text NOT NUL
     ->applyToPalette('accordionSingle', 'tl_content');
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['isFAQMarkup'] = [
-    'label' => ['Diesen Inhalt in die FAQPage aufnehmen', ''],
+    'label' => ['Diesen Inhalt mit schema.org als FAQ auszeichnen', ''],
     'inputType' => 'checkbox',
     'eval' => [
         'tl_class' => 'm12 w50'
